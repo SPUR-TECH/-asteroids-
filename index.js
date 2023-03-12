3eb3038537dde5d36f37e54995b7e2ab7953fb48
Scott Atkinson <scott.atkin79@gmail.com>
2023-03-12 15:51:18 +0000

    add shoot sound to touch controls

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function spawnPowerUps() {
                 }
             })
         )
-    }, 10000)
+    }, 50000)
 }
 
 function createScoreLabel({
@@ -296,12 +296,15 @@ addEventListener('click', (event) => {
 
 addEventListener('touch', (event) => {
     event.preventDefault()
-    const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x)
-    const velocity = {
-        x: Math.cos(angle) * 5,
-        y: Math.sin(angle) * 5
+    if (game.active) {
+        const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x)
+        const velocity = {
+            x: Math.cos(angle) * 5,
+            y: Math.sin(angle) * 5
+        }
+        projectiles.push(new Projectile(player.x, player.y, 5, 'white', velocity))
+        audio.shoot.play()
     }
-    projectiles.push(new Projectile(player.x, player.y, 5, 'white', velocity))
 })
 
 const mouse = {
